d06e13972f9d86eed923284a55d86d83e32d767f
Nick Cannariato <434063+birdcar@users.noreply.github.com>
2021-08-29 16:42:37 -0500

    Complete activity 14

--- activities/src/bin/a14.rs
+++ activities/src/bin/a14.rs
@@ -11,4 +11,40 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+struct Person {
+  age: u8,
+  name: String,
+  color: String,
+}
+
+impl Person {
+  fn print(&self) {
+    println!("name: {:?}\ncolor: {:?}\n", self.name, self.color)
+  }
+}
+
+fn main() {
+  let people = vec![
+    Person {
+      age: 10,
+      name: String::from("Jay"),
+      color: String::from("Green"),
+    },
+    Person {
+      age: 33,
+      name: String::from("Nick"),
+      color: String::from("Black"),
+    },
+    Person {
+      age: 32,
+      name: String::from("Dani"),
+      color: String::from("Purple"),
+    },
+  ];
+
+  for person in &people {
+    if person.age < 33 {
+      person.print();
+    }
+  }
+}
