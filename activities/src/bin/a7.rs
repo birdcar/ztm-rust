5a4827b111bebd83d01a19ef7de25cb77b706dd0
Nick Cannariato <434063+birdcar@users.noreply.github.com>
2021-08-29 13:48:28 -0500

    Complete activity 7

--- activities/src/bin/a7.rs
+++ activities/src/bin/a7.rs
@@ -5,9 +5,31 @@
 //
 // Notes:
 // * Use an enum with color names as variants
+enum Colors {
+  Red,
+  Orange,
+  Yellow,
+  Green,
+  Blue,
+  Indigo,
+  Violet,
+}
+
 // * Use a function to print the color name
 // * The function must use the enum as a parameter
-// * Use a match expression to determine which color
-//   name to print
+fn print_color(color: Colors) {
+  // * Use a match expression to determine which color name to print
+  match color {
+    Colors::Red => println!("Red"),
+    Colors::Orange => println!("Orange"),
+    Colors::Yellow => println!("Yellow"),
+    Colors::Green => println!("Green"),
+    Colors::Blue => println!("Blue"),
+    Colors::Indigo => println!("Indigo"),
+    Colors::Violet => println!("Violet"),
+  }
+}
 
-fn main() {}
+fn main() {
+  print_color(Colors::Red);
+}
