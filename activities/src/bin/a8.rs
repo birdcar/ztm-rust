e45e25a237ff462d81f0ab7da6b25b3fbb5ccd5b
Nick Cannariato <434063+birdcar@users.noreply.github.com>
2021-08-29 13:48:45 -0500

    Complete activity 8

--- activities/src/bin/a8.rs
+++ activities/src/bin/a8.rs
@@ -8,5 +8,32 @@
 // * Use a struct to store drink flavor and fluid ounce information
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
+enum Flavor {
+  Lime,
+  Lemon,
+  PricklyPear,
+}
 
-fn main() {}
+struct Drink {
+  flavor: Flavor,
+  fluid_oz: f64,
+}
+
+fn print_drink_data(drink: Drink) {
+  let flavor = match drink.flavor {
+    Flavor::Lime => String::from("Lime"),
+    Flavor::Lemon => String::from("Lemon"),
+    Flavor::PricklyPear => String::from("Prickly Pear"),
+  };
+
+  println!("{:?} ({:?} oz)", flavor, drink.fluid_oz);
+}
+
+fn main() {
+  let prickly_pear_pint = Drink {
+    flavor: Flavor::PricklyPear,
+    fluid_oz: 16.0,
+  };
+
+  print_drink_data(prickly_pear_pint);
+}
